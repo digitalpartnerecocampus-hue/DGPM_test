8e5606b2e5ba3162bc13de7e2311daf7e54babb8
digitalpartnerecocampus-hue <digitalpartnerecocampus@gmail.com>
2026-01-06 21:07:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,17 +4,8 @@ const CONFIG = {
     supabaseUrl: "https://rjzaikcjuycmecxgifzg.supabase.co",
     supabaseKey: "sb_publishable_gdLqam4Fu7BPGBwBnA5cuQ_C2RPhzgq",
 
-    // 2. FIREBASE (Auth)
-    firebaseConfig: {
-        apiKey: "AIzaSyAVH7EcQrBwgoSgghOQ87LM-e1-h4LXOl8",
-        authDomain: "dgpm-d862c.firebaseapp.com",
-        projectId: "dgpm-d862c",
-        storageBucket: "dgpm-d862c.firebasestorage.app",
-        messagingSenderId: "659776836882",
-        appId: "1:659776836882:web:6c3df3f12960d6127acfcb"
-    },
 
-    // 3. CLOUDINARY (Image Upload)
+    // 2. CLOUDINARY (Image Upload)
     cloudinaryCloudName: "da1phbsy0",
     cloudinaryUploadPreset: "Medias" // Create an "Unsigned" preset in Cloudinary settings
 };
